8b846ed5eacaa40cbe7266a64b52f9ab64425779
Sabrina Beck Angelini <sabrinabeck.96@gmail.com>
2016-11-02 18:13:32 -0200

    fix test 4

--- python/teste4.py
+++ python/teste4.py
@@ -1,11 +1,11 @@
 def contaOcorrencias(string):
     if type(string) is not str:
         raise TypeError("Tipo inesperado.")
-    regex = "[A-Za-zàÀáÁäÄâÂãÃèÈéÉëËêÊẽẼìÌíÍïÏîÎĩĨòÒóÓöÖôÔõÕùÙúÚüÜûÛũŨçÇ]{6,}(?\.|,|!|?|\.\.\.)?"
+    regex = "[A-Za-zàÀáÁäÄâÂãÃèÈéÉëËêÊẽẼìÌíÍïÏîÎĩĨòÒóÓöÖôÔõÕùÙúÚüÜûÛũŨçÇ]{6,}"
     import re
     palavras = re.findall(regex, string)
     ocorrencias = {}
     for palavra in palavras:
         palavra = palavra.replace(".", "").replace(",", "").replace("!", "").replace("?", "")
-        ocorrencias[palavra] = ocorrencias.get(palavra, 0)
+        ocorrencias[palavra] = ocorrencias.get(palavra, 0) + 1
     return ocorrencias
